7de419dc4097299e1c790c949efa77320fdb49c0
thedavidhk <herok.davidj@gmail.com>
2020-02-21 20:58:53 +0000

    Added policy iteration algorithm

--- dsge/dynamic_system.py
+++ dsge/dynamic_system.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import matplotlib
+# import matplotlib
+from functools import lru_cache
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import interp
+import pickle
 from scipy.optimize import fminbound
 
-matplotlib.use('tkagg')
+# matplotlib.use('tkagg')
 
 
 class LinInterp:
@@ -16,7 +17,6 @@ class LinInterp:
         X (iterable): Points in first dimension.
         Y (iterable): Points in second dimension.
     """
-
     def __init__(self, X, Y):
         """__init__
 
@@ -70,7 +70,7 @@ class Dynamic_Optimization(object):
         s_space (np.meshgrid): State space.
         discount (float): Discount factor (0 < time_discount < 1).
         transformation (callable): Transformation to next state given action.
-        """
+    """
     def __init__(self, objective, s_space, time_discount, transformation):
         """__init__
 
@@ -101,8 +101,23 @@ class Dynamic_Optimization(object):
             float: Value of the current state and action based on vf_old.
         """
         current_reward = self.objective(action, state)
-        future_reward = vf_old(self.transformation(state, action))
+        future_reward = vf_old(self.transformation(action, state))
         return current_reward + self.discount * future_reward
+
+    def policy(self, v):
+        """Determine the optimal policy given a value function
+
+        Args:
+            v (callable): Value function.
+
+        Returns:
+            callable: Optimal policy function.
+        """
+        vals = []
+        for s in self.s_space:
+            xmax = s**0.5
+            vals.append(maximize(self.value, 1e-2, xmax, (s, v))[1])
+        return LinInterp(self.s_space, vals)
 
     def bellman(self, vf_old):
         """Bellman operator
@@ -120,10 +135,11 @@ class Dynamic_Optimization(object):
         vals = []
         for s in self.s_space:
             # TODO: make this budget constraint more explicit and general
-            xmax = s[0]
-            vals.append(maximize(self.value, 0, xmax, (s, vf_old))[0])
+            xmax = s**0.5
+            vals.append(maximize(self.value, 1e-2, xmax, (s, vf_old))[0])
         return LinInterp(self.s_space, vals)
 
+    @lru_cache(maxsize=8)
     def value_function_iteration(self, v0=None, tol=1e-5, maxiter=200):
         """Value function iteration
 
@@ -141,49 +157,86 @@ class Dynamic_Optimization(object):
         if v0 is None:
 
             def v0(x):
-                return x
+                return 0
 
         v = v0
-        count = 0
         msd = tol + 1  # Mean squared difference. Initialized > tol.
-        while count < maxiter and msd > tol:
+        for i in range(maxiter):
             v_old = v
-            v = model.bellman(v)
-            msd = sum((v(s_space) - v_old(s_space))**2) / len(s_space)
-            if count % 10 == 0:
-                print("count: %d; mse: %f" % (count, msd))
-            count += 1
-        if count <= 200:
-            print("Convergence after %d iterations." % count)
-        else:
-            print("Convergence not reached after %d iterations." % maxiter)
+            v = self.bellman(v)
+            msd = sum(
+                (v(self.s_space) - v_old(self.s_space))**2) / len(self.s_space)
+            if (i + 1) % 10 == 0:
+                print("count: %d; mse: %f" % (i + 1, msd))
+            if msd < tol:
+                print("Convergence after %d iterations." % (i + 1))
+                break
+            if i == maxiter - 1:
+                print("Convergence not reached after %d iterations." % maxiter)
         return v
 
-    def policy(self, v):
-        """Determine the optimal policy given a value function
+    def policy_value(self, pi, tol=1e-5):
+        def v(x):
+            return 0
+
+        maxiter = 100
+        for i in range(maxiter):
+            v_old = v
+            vals = []
+            for s in self.s_space:
+                action = pi(s)
+                vals.append(self.value(action, s, v_old))
+            v = LinInterp(self.s_space, vals)
+            msd = sum(
+                (v(self.s_space) - v_old(self.s_space))**2) / len(self.s_space)
+            if msd < tol:
+                # value converged
+                break
+        return v
+
+    def policy_iteration(self, pi0=None, tol=1e-5, maxiter=200):
+        """Policy iteration.
 
         Args:
-            v (callable): Value function.
+            p0 (callable, optional): Inital guess for the policy function.
+            tol (float): Tolerance for convergence criterion. If the mean
+                squared difference between two iterations is less than tol,
+                break the loop and consider the value function converged.
 
         Returns:
-            callable: Optimal policy function.
+            callable: Converged policy function.
         """
-        vals = []
-        for s in self.s_space:
-            xmax = s
-            vals.append(maximize(self.value_function, 0, xmax, (s, v))[1])
-        return LinInterp(self.s_space, vals)
+        if pi0 is None:
+
+            def pi0(x):
+                return 0
+
+        pi = pi0
+        msd = tol + 1  # Mean squared difference. Initialized > tol.
+        for i in range(maxiter):
+            v_pi_old = self.policy_value(pi)
+            pi_new = self.policy(v_pi_old)
+            msd = sum((pi_new(self.s_space) - pi(self.s_space))**2) / len(
+                self.s_space)
+            if (i + 1) % 10 == 0:
+                print("count: %d; mse: %f" % (i + 1, msd))
+            if msd < tol:
+                print("Convergence after %d iterations." % (i + 1))
+                break
+            if i == maxiter - 1:
+                print("Convergence not reached after %d iterations." % maxiter)
+            pi = pi_new
+        return pi
 
 
 if __name__ == "__main__":
 
     delta = 0.1
-    alpha = 0.4
-    beta = 0.90
+    alpha = 0.5
+    beta = 0.95
     n = 0.05
-    kap = np.linspace(1e-1, 20, 100)
-    lab = np.linspace(1e-1, 20, 100)
-    s_space = [kap, lab]
+    kap = np.linspace(1e-1, 20, 200)
+    s_space = kap
 
     def f(kap, lab, alpha=0.5):
         return kap**alpha * lab**(1 - alpha)
@@ -191,23 +244,40 @@ if __name__ == "__main__":
     def u(x, s):
         return np.log(x)
 
-    def s(x, s_old):
-        k_old = s_old[0]
-        l_old = s_old[1]
-        kap = k_old * (1 - delta) + f(k_old, l_old) - x
-        lab = l_old * (1 + n)
-        return [kap, lab]
+    def k_new(c, k):
+        return k * (1 - delta) + f(k, 1) - c
 
-    model = Dynamic_Optimization(u, s_space, beta, s)
-    v_star = model.value_function_iteration()
+    model = Dynamic_Optimization(u, s_space, beta, k_new)
+
+    try:
+        with open("piter.pyobj", "rb") as piter:
+            c_star = pickle.load(piter)
+        print("File loaded")
+    except FileNotFoundError:
+        c_star = model.policy_iteration(tol=1e-6)
+        with open("piter.pyobj", "wb") as piter:
+            pickle.dump(c_star, piter)
+        print("File not found. Created new cache file piter.pyobj.")
 
     # Optimal policy:
-    pi_star = model.policy(v_star)
+    # c_star = model.policy(v_star)
 
-    # Show in plot:
+    # Simulate time series
+    T = 100
+    k = []
+    y = []
+    c = []
+    k.append(1)
+
+    for t in range(T):
+        y.append(f(k[t], 1))
+        c.append(c_star(k[t]))
+        if t < T - 1:
+            k.append(k_new(c[t], k[t]))
+
     fig, ax = plt.subplots()
-    ax.set_xlim(s_space.min(), s_space.max())
-    ax.plot(s_space, pi_star(s_space))
+    ax.plot(range(T), y, label="y")
+    ax.plot(range(T), c, label="c")
+    ax.plot(range(T), k, label="k")
+    ax.legend(loc='lower right')
     plt.show()
-
-    # plt.show()
